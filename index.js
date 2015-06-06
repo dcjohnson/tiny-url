1b17db22f0c9ac5a19aae94ba21d3f4efe3f077a
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2015-06-06 16:38:29 -0600

    halt work on this while I write the regex library.  Also, need to work on lisp interpreter.

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-UrlTree = function() {
+UrlTreeNode = function(children, urlPoint, isRegex, handler) {
+	this.children = [children];
+	this.urlPoint = urlPoint;
+	this.isRegex = isRegex;
+	this.handler = handler;
+};
+
+// UrlTreeNode.prototype.compareUrlPoints = function(urlPoint) {
+// 	if(this.isRegex === true) {
+//
+// 	} else {
+// 		return this.urlPoint === urlPoint;
+// 	}
+// }
+
+UrlTree = function(head, httpVerb) {
+	this.head = head;
+	this.httpVerb = httpVerb;
+};
+
+UrlTree.prototype.addEndPoint = function(points, handler) {
 
 }
 
@@ -6,6 +26,6 @@ exports.Route = function() {
 	this.endPoints = new UrlTree();
 };
 
-exports.Route.prototype.addEndPoint = function(route, handler) {
+exports.Route.prototype.newRout = function(route, handler) {
 
 };
