24b954da36c0e60e9da508ec77f88f411fb8dda4
Dylan Conrad Johnson <djohnson@ancestry.com>
2015-07-05 16:15:53 -0600

    started work on the router and detected bug in the regex parser.

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
-var ndfa = require('tiny-url-regex');
+var regex = require('tiny-url-regex');
 
-UrlTreeNode = function(children, urlPoint, handler) {
-    this.children = children;
-    this.urlPoint = urlPoint;
-    this.handler = handler;
+Router = function() {
+    this.getEndpoints = [];
+    this.postEndpoints = [];
+    this.addEndpoint = function(endpoints, path, handler) {
+
+    }
 }
 
-UrlTree = function(head, httpVerb) {
-    this.head = head;
-    this.httpVerb = httpVerb;
+Router.prototype.get = function(path, handler) {
+    this.addEndpoint(this.getEndpoints, path, handler);
 }
 
-UrlTree.prototype.addEndPoint = function(points, handler) {
-
+Router.prototype.post = function(path, handler) {
+    this.addEndpoint(this.postEndpoints, path, handler);
 }
 
-exports.Route = function() {
-    this.endPoints = new UrlTree();
-}
-
-exports.Route.prototype.newRout = function(route, handler) {
+Router.prototype.addRouter = function(router) {
 
 }
 
+Router.prototype.use = function(middleWare) {
 
-var state = new ndfa.Ndfa('abc');
-state.generateStates();
-console.log(state.testString('abc'));
-console.log(state.testString('abcc'));
+}
+
+Router.prototype.apply = function(req, res) {
+
+}
+
+Endpoint = function(endpointHandler, regex, parentRouter) {
+    this.router = parentRouter;
+    this.handler = endpointHandler;
+    this.childEndpoints = []
+    this.regex = regex;
+    this.regex.generateStates();
+}
+
+Endpoint.prototype.applyUrl = function(path) {
+
+}
+
+Endpoint.prototype.testUrl = function(path) {
+    return this.regex.testString(path);
+}
+
+Endpoint.prototype.addChildEndpoint = function(endPoint) {
+    this.childEndpoints.push(endPoint);
+}
+
+
+
+var r = new Router();
+
+r.get('a', 'b');
+
+exports.Router = Router;
+exports.Endpoint = Endpoint;
+
+var ndfa = new regex.Ndfa('ab[a|b]san334bnljslj4blsj[a|b|09|s|e|g]A[a|b|9|s|e|g]cA[a|b|9|s|e|g]cA[a|b|9|s|e|g]cA[a|b|9|s|e|g]cA[a|b|9|s|e|g]c');
+ndfa.generateStates();
+console.log(ndfa.testString('abaaac'));
+console.log(ndfa.testString('abbbaababbc'));
