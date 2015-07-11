30856feeb0018a6fb57261aa746f0728e7998530
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2015-07-11 15:50:16 -0600

    update, new idea

--- index.js
+++ index.js
@@ -3,28 +3,10 @@ var regex = require('tiny-url-regex');
 Router = function() {
     this.getEndpoints = [];
     this.postEndpoints = [];
+    this.externController = [];
+    this.middleWare = [];
     this.addEndpoint = function(endpoints, path, handler) {
-        var pathSegments = path.split('/');
-        var paths = endpoints.filter(function(elem, index, array) {
-            return elem.testUrl(pathSegments[0]);
-        });
-        var path = paths[0];
-        var pathExists = paths.length === 1;
-        for (var index = 1; index < pathSegments.length; index++) {
-            if (path.hasMatchingChildEndpoint(pathSegments[index])) {
-                path = path.getMatchingChildEndpoint(pathSegments[index]);
-            } else {
-                var ndfa = new regex.Ndfa(pathSegments[index]);
-                ndfa.generateStates();
-                if (index + 1 === pathSegments.length) {
-                    var newEndpoint = new Endpoint(ndfa, this, handler);
-                } else {
-                    var newEndpoint = new Endpoint(ndfa, this);
-                }
-                path.addEndpoint(newEndpoint);
-                path = newEndpoint;
-            }
-        }
+        
     }
 }
 
@@ -41,57 +23,60 @@ Router.prototype.addRouter = function(router) {
 }
 
 Router.prototype.use = function(middleWare) {
-
+    this.middleWare.push(middleWare);
 }
 
 Router.prototype.apply = function(req, res) {
-
+    for(var index = 0; index < this.middleWare.length; index++) {
+        this.middleWare[index](req, res);
+    }
 }
 
-Endpoint = function(regex, parentRouter, endpointHandler) {
-    this.router = parentRouter;
+UrlCheck = function(regexRule, checkFunc) {
+    this.ndfa = new regex.Ndfa(regexRule);
+    this.regex.generateStates();
+    this.checkFunc = checkFunc;
+}
+
+UrlCheck.prototype.testUrlSegment = function(urlSeg, req, res) {
+    var stringMatch = this.ndfa.testString(urlSeg);
+    if (typeof this.checkFunc !== 'undefined') {
+        return stringMatch && this.checkFunc(req, res);
+    }
+    return stringMatch;
+}
+
+Endpoint = function(urlChecker, endpointHandler) {
     this.handler = endpointHandler;
     this.childEndpoints = [];
-    this.regex = regex;
-    this.regex.generateStates();
+    this.urlChecker = urlChecker;
 }
 
-Endpoint.prototype.applyUrl = function(path) {
+Endpoint.prototype.applyUrl = function(urlSeg, req, res) {
 
 }
 
-Endpoint.prototype.testUrl = function(path) {
-    return this.regex.testString(path);
+Endpoint.prototype.testUrl = function(urlSeg, req, res) {
+    return this.urlChecker.testUrlSegment(urlSeg, req, res);
 }
 
 Endpoint.prototype.addChildEndpoint = function(endPoint) {
     this.childEndpoints.push(endPoint);
 }
 
-Endpoint.prototype.getMatchingChildEndpoint = function(urlSegment) {
+Endpoint.prototype.getMatchingChildEndpoint = function(urlSeg) {
     var match = childEndpoints.filter(function(elem, index, array) {
-        return elem.testUrl(urlSegment);
+        return elem.testUrl(urlSeg);
     });
     return match[0];
 }
 
-Endpoint.prototype.hasMatchingChildEndpoint = function(urlSegment) {
+Endpoint.prototype.hasMatchingChildEndpoint = function(urlSeg) {
     var matches = childEndpoints.filter(function(elem, index, array) {
-        return elem.testUrl(urlSegment);
-    })
+        return elem.testUrl(urlSeg);
+    });
     return matches.length === 1;
 }
 
-
-
-var r = new Router();
-
-r.get('a', 'b');
-
 exports.Router = Router;
 exports.Endpoint = Endpoint;
-
-var ndfa = new regex.Ndfa('ab[a|b]san334bnljslj4blsj[a|b|09|s|e|g]A[a|b|9|s|e|g]cA[a|b|9|s|e|g]cA[a|b|9|s|e|g]cA[a|b|9|s|e|g]cA[a|b|9|s|e|g]c');
-ndfa.generateStates();
-console.log(ndfa.testString('abaaac'));
-console.log(ndfa.testString('abbbaababbc'));
