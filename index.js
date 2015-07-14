f219b612ec03bcb9e41200db426b22912ae672e3
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2015-07-13 23:57:13 -0600

    I think I finally have areal design down.  I have the endpoint logic decided upon and finished. Now I need to do the router.  This should be very simple.

--- index.js
+++ index.js
@@ -1,82 +1,96 @@
 var regex = require('tiny-url-regex');
 
 Router = function() {
-    this.getEndpoints = [];
-    this.postEndpoints = [];
-    this.externController = [];
-    this.middleWare = [];
-    this.addEndpoint = function(endpoints, path, handler) {
-        
+    var urls = [];
+    var subControllers = [];
+    var middleWare = [];
+
+    this.addEndpoint = function(verb, path, handler) {
+
+    }
+
+    this.get = function(path, handler) {
+
+    }
+
+    this.post = function(path, handler) {
+
+    }
+
+    this.addSubController = function(subController) {
+
+    }
+
+    this.use = function(middleWare) {
+        this.middleWare.push(middleWare);
+    }
+
+    this.apply = function(req, res) {
+        for(var index = 0; index < this.middleWare.length; index++) {
+            this.middleWare[index](req, res);
+        }
     }
 }
 
-Router.prototype.get = function(path, handler) {
-    this.addEndpoint(this.getEndpoints, path, handler);
-}
+UrlCheck = function(regexRule, checkFunc, depth) {
+    var ndfa = new regex.Ndfa(regexRule);
+    ndfa.generateStates();
+    var checkFunc = checkFunc;
+    var depth = depth;
 
-Router.prototype.post = function(path, handler) {
-    this.addEndpoint(this.postEndpoints, path, handler);
-}
-
-Router.prototype.addRouter = function(router) {
-
-}
-
-Router.prototype.use = function(middleWare) {
-    this.middleWare.push(middleWare);
-}
-
-Router.prototype.apply = function(req, res) {
-    for(var index = 0; index < this.middleWare.length; index++) {
-        this.middleWare[index](req, res);
+    this.testUrlSeg = function(req, res) {
+        var urlSeg = req.url.split('/');
+        var stringMatch = this.ndfa.testString(urlSeg);
+        if (typeof this.checkFunc !== 'undefined') {
+            return stringMatch && checkFunc(req, res);
+        }
+        return stringMatch;
     }
-}
-
-UrlCheck = function(regexRule, checkFunc) {
-    this.ndfa = new regex.Ndfa(regexRule);
-    this.regex.generateStates();
-    this.checkFunc = checkFunc;
-}
-
-UrlCheck.prototype.testUrlSegment = function(urlSeg, req, res) {
-    var stringMatch = this.ndfa.testString(urlSeg);
-    if (typeof this.checkFunc !== 'undefined') {
-        return stringMatch && this.checkFunc(req, res);
-    }
-    return stringMatch;
 }
 
 Endpoint = function(urlChecker, endpointHandler) {
-    this.handler = endpointHandler;
-    this.childEndpoints = [];
-    this.urlChecker = urlChecker;
-}
+    var handler = endpointHandler;
+    var childEndpoints = [];
+    var urlChecker = urlChecker;
+    var pageNotFound = function(res) {
+        res.write('404: Page not found.');
+        res.end();
+    }
 
-Endpoint.prototype.applyUrl = function(urlSeg, req, res) {
+    this.applyUrl = function(req, res) {
+        if (this.testUrl(req, res)) {
+            if (typeof handler !== 'undefined') {
+                handler(req, res);
+            } else {
+                var nextEndpoint = this.getNextEndpoint(req, res);
+                if (nextEndpoint) {
+                    nextEndpoint.applyUrl(req, res);
+                }
+            }
+        } else {
+            pageNotFound(res);
+        }
+    }
 
-}
+    this.set404 = function(func404) {
+        this.pageNotFound = func404;
+    }
 
-Endpoint.prototype.testUrl = function(urlSeg, req, res) {
-    return this.urlChecker.testUrlSegment(urlSeg, req, res);
-}
+    this.testUrl = function(req, res) {
+        return this.urlChecker.testUrlSeg(req, res);
+    }
 
-Endpoint.prototype.addChildEndpoint = function(endPoint) {
-    this.childEndpoints.push(endPoint);
-}
+    this.addChildEndpoint = function(endPoint) {
+        this.childEndpoints.push(endPoint);
+    }
 
-Endpoint.prototype.getMatchingChildEndpoint = function(urlSeg) {
-    var match = childEndpoints.filter(function(elem, index, array) {
-        return elem.testUrl(urlSeg);
-    });
-    return match[0];
-}
-
-Endpoint.prototype.hasMatchingChildEndpoint = function(urlSeg) {
-    var matches = childEndpoints.filter(function(elem, index, array) {
-        return elem.testUrl(urlSeg);
-    });
-    return matches.length === 1;
+    this.getNextEndpoint = function(req, res) {
+        return childEndpoints.filter(function(elem) {
+            return elem.testUrl(req, res);
+        })[0];
+    }
 }
 
 exports.Router = Router;
 exports.Endpoint = Endpoint;
+exports.UrlCheck = UrlCheck;
